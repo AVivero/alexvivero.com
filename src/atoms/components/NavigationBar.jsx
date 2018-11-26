c41ef56bc8c3f6f254d48d456ef8327fed8de265
Alex Vivero <alexvivero@Alexs-MacBook-Pro.local>
2018-11-25 22:56:46 -0500

    update navigation bar to include tooltips

--- src/atoms/components/NavigationBar.jsx
+++ src/atoms/components/NavigationBar.jsx
@@ -5,21 +5,61 @@ import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
 import Grid from '@material-ui/core/Grid';
+import Tooltip from '@material-ui/core/Tooltip';
+import Zoom from '@material-ui/core/Zoom';
+import { isAbsolute } from 'path';
 
 const styles = theme => ({
   navigationButton: {
     backgroundColor: 'tranparent',
     color: 'rgb(31, 30, 28)',
+    fontSize: 16,
+    minWidth: 60,
+    minHeight: 60,
+    borderRadius: 0,
+  },
+  navigationButtonIcon: {
+    fontSize: 25,
+    fontWeight: 'normal',
   },
   navigationBar: {
     backgroundColor: 'rgb(255, 217, 62)',
-    maxWidth: 65,
+    maxWidth: 60,
     position: 'fixed',
     top: 30,
-    left: 20,
+    left: '5vw',
+    boxShadow: 'rgba(62, 62, 62, 0.3) 0px 0px 80px 0px',
   },
   navigationItem: {
     listStyle: 'none',
+  },
+  activeNavigationItem: {
+    backgroundColor: '#fff',
+  },
+  navigationItemTooltip: {
+    background: '#fff',
+    color: 'rgb(31, 30, 28)',
+    textTransform: 'uppercase',
+    height: 40,
+    width: 100,
+    fontSize: 14,
+    fontWeight: 'bold',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 0,
+    '&::before': {
+      content: '""',
+      margin: 'auto',
+      display: 'block',
+      width: 0,
+      height: 0,
+      position: 'absolute',
+      left: -8,
+      borderStyle: 'solid',
+      borderWidth: '1em 1em 1em 0',
+      borderColor: 'transparent #fff transparent transparent',
+    },
   },
 });
 
@@ -30,16 +70,22 @@ const handleNavigation = (event) => {
 const renderNavigationItems = (items, classes) => {
   return items.map((item) => {
     return (
-      <Grid item xs={1} key={item.caption}>
-        <li className={`${classes.navigationItem} navigation-item ${item.active ? 'active' : ''}`}>
-          <Button
-            className={classes.navigationButton}
-            aria-label={item.caption}
-            onClick={handleNavigation}
-          >
-            <Icon>{item.icon || 'navigation'}</Icon>
-          </Button>
-        </li>
+      <Grid item xs={12} key={item.caption}>
+        <Tooltip title={item.caption} placement="right" TransitionComponent={Zoom} classes={{ tooltip: classes.navigationItemTooltip }}>
+          <li className={`${classes.navigationItem} navigation-item ${item.active && classes.activeNavigationItem}`}>
+            <Button
+              className={`${classes.navigationButton}`}
+              aria-label={item.caption}
+              onClick={handleNavigation}
+            >
+              <Icon
+                className={`${classes.navigationButtonIcon}`}
+              >
+                {item.icon || 'navigation'}
+              </Icon>
+            </Button>
+          </li>
+        </Tooltip>
       </Grid>
     );
   });
